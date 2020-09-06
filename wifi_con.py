1b28db02616630221b526ee8786b642174ced2ac
Klork-droid <60038225+Klork-droid@users.noreply.github.com>
2020-09-06 16:35:20 +0300

    add wifi_con

---
+++ wifi_con.py
@@ -0,0 +1,47 @@
+import subprocess
+import time
+
+
+def check_result(profile):
+    result = subprocess.run(f"netsh wlan connect name={profile}", shell=True, stdout=subprocess.PIPE)
+    result = result.stdout.decode('cp866', 'ignore')
+    return result
+
+
+def check_ping():
+    ping = subprocess.run("ping 2miners.com", shell=True, stdout=subprocess.PIPE)
+    ping = ping.stdout.decode('cp866', 'ignore')
+    return ping
+
+
+profile = subprocess.run("netsh wlan show profile", shell=True, stdout=subprocess.PIPE)
+profile = profile.stdout.decode('cp866', 'ignore')
+profile_list = []
+with open('wifi_list.txt', 'w+', encoding='utf-8') as file:
+    file.write(profile)
+    file.seek(0)
+    for line in file.readlines():
+        if 'Все профили пользователей' in line:
+            start = line.find(':') + 2
+            profile_list.append(line[start:].strip())
+
+while True:
+    ping = check_ping()
+    interface = subprocess.run("netsh wlan show interface", shell=True, stdout=subprocess.PIPE)
+    interface = interface.stdout.decode('cp866', 'ignore')
+    start = interface.find('SSID')
+    end = interface.find('\n', start)
+    ssid = interface[start: end].strip()
+    if 'Обмен пакетами' in ping:
+        print(f'Connected to {ssid}, next sleep')
+        time.sleep(60)
+    else:
+        for profile in profile_list:
+            result = check_result(profile)
+            new_ping = check_ping()
+            print(f'Подключение к {profile} : {result[:-2]}')
+            if 'Обмен пакетами' in new_ping:
+                print(f'Доступ к сети {profile} : YES')
+                break
+            else:
+                print(f'Доступ к сети {profile} : NO')
